638d7e89e5ba46e439f46453bd6ebfe4b9648e56
Rudi <rudi.will.do.it@gmail.com>
2023-12-11 00:20:39 +0300

    add hard test

---
+++ src/test/java/com/brash/hard/RandomHardTestSettings.java
@@ -0,0 +1,11 @@
+package com.brash.hard;
+
+public interface RandomHardTestSettings {
+    int NUMBER_ITEMS = 100;
+    int NUMBER_USERS = 100;
+
+    int MAX_PERCENT_MARKED_ITEMS = 5;
+
+    int MAX_MARK = 10;
+    int MIN_MARK = 1;
+}
---
+++ src/test/java/com/brash/hard/RandomUtils.java
@@ -0,0 +1,24 @@
+package com.brash.hard;
+
+import com.brash.data.entity.Item;
+
+import java.util.List;
+import java.util.Random;
+
+public class RandomUtils {
+
+    private Random random = new Random();
+
+    public int getRandomInt(int min, int max) {
+        return random.nextInt(max - min) + min;
+    }
+
+    public Item getRandomUniqueItem(List<Item> items, List<Item> markedItems) {
+        while (true) {
+            int index = getRandomInt(0, items.size());
+            Item item = items.get(index);
+            if (!markedItems.contains(item))
+                return item;
+        }
+    }
+}
