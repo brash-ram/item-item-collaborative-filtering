bea98ec01e9c9d76b7c047d6880e0684502b882d
Rudi <rudi.will.do.it@gmail.com>
2023-12-12 23:52:24 +0300

    add gauss test

--- src/test/java/com/brash/hard/random/RandomHardTestSettings.java
+++ src/test/java/com/brash/hard/random/RandomHardTestSettings.java
@@ -1,10 +1,10 @@
-package com.brash.hard;
+package com.brash.hard.random;
 
 public interface RandomHardTestSettings {
     int NUMBER_ITEMS = 100;
-    int NUMBER_USERS = 1000;
+    int NUMBER_USERS = 100;
 
-    int MAX_PERCENT_MARKED_ITEMS = 15;
+    int MAX_PERCENT_MARKED_ITEMS = 5;
 
     int MAX_MARK = 10;
     int MIN_MARK = 1;
