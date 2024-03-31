5d37d6f146d5645c6d6d6a071edae5660c58182e
Rudi <rudi.will.do.it@gmail.com>
2024-03-31 13:29:43 +0300

    fix mapping rabbit

--- src/main/java/com/brash/config/ApplicationConfiguration.java
+++ src/main/java/com/brash/config/ApplicationConfiguration.java
@@ -1,13 +1,20 @@
 package com.brash.config;
 
+import com.brash.dto.rabbit.RabbitItemDTO;
+import com.brash.dto.rabbit.RabbitMarkDTO;
+import com.brash.dto.rabbit.RabbitUserDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.ClassMapper;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -58,7 +65,22 @@ public class ApplicationConfiguration {
     }
 
     @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public ClassMapper classMapper() {
+        Map<String, Class< ? >> mappings = new HashMap<>();
+        mappings.put(rabbitMQConfig.addItemClass(), RabbitItemDTO.class);
+        mappings.put(rabbitMQConfig.addUserClass(), RabbitUserDTO.class);
+        mappings.put(rabbitMQConfig.addMarkClass(), RabbitMarkDTO.class);
+
+        DefaultClassMapper classMapper = new DefaultClassMapper();
+        classMapper.setTrustedPackages("com.brash.dto.rabbit.*");
+        classMapper.setIdClassMapping(mappings);
+        return classMapper;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(ClassMapper classMapper) {
+        Jackson2JsonMessageConverter jsonConverter = new Jackson2JsonMessageConverter();
+        jsonConverter.setClassMapper(classMapper);
+        return jsonConverter;
     }
 }
